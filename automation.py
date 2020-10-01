0a47ccae2ab4fddf2c4d4aa8c66959ab2313f6c5
Rocketeer7 <68909112+Rocketeer-7@users.noreply.github.com>
2020-10-02 00:48:38 +0530

    Update automation.py
    Added Comments.

--- automation.py
+++ automation.py
@@ -1,24 +1,24 @@
-import pyautogui 
-from PIL import Image, ImageGrab 
-
+import pyautogui                   #import python auto graphical user interface library
+from PIL import Image, ImageGrab   #Import Image and Image Grab from Python Imaging library
+#from numpy import asarray
 import time
 
-def hit(key):
-    pyautogui.keyDown(key)
+def hit(key):              #function for using down key
+    pyautogui.keyDown(key)  
     return
 
-def isCollide(data):
+def isCollide(data):       #To check a rectangular area for birds
  
     for i in range(300, 415):
         for j in range(410, 563):
             if data[i, j] < 100:
-                hit("down")
+                hit("down")         #Hit down if birds are present
                 return
 
-    for i in range(300, 415):
+    for i in range(300, 415):     #To check a rectangular area for Cacti
         for j in range(563, 650):
             if data[i, j] < 100:
-                hit("up")
+                hit("up")              #Hit up to jump when cacti are present
                 return
     return
 
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # hit('up') 
 
     while True:
-        image = ImageGrab.grab().convert('L')  
+        image = ImageGrab.grab().convert('L')  #Loop to process images and find information about cacti and birds
         data = image.load()
         isCollide(data)
             
